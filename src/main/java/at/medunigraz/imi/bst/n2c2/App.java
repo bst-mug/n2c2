440a041cc56b0ba76e2ccb4233df8e2bb7ce3447
Michel Oleynik <michelole@gmail.com>
2018-03-13 16:01:03 +0100

    Create maven structure using maven-archetype-quickstart

---
+++ src/main/java/at/medunigraz/imi/bst/n2c2/App.java
@@ -0,0 +1,13 @@
+package at.medunigraz.imi.bst.n2c2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
