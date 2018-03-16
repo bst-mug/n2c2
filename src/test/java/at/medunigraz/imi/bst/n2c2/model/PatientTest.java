d1fa634cd8a1639c6d3036b9dfa140eb311c6cac
Michel Oleynik <michelole@gmail.com>
2018-03-16 19:48:34 +0100

    Exercise all criteria (fixes #6)

--- src/test/java/at/medunigraz/imi/bst/n2c2/model/PatientTest.java
+++ src/test/java/at/medunigraz/imi/bst/n2c2/model/PatientTest.java
@@ -15,5 +15,16 @@ public class PatientTest {
 
         assertEquals(Eligibility.NOT_MET, patient.getEligibility(Criterion.ABDOMINAL));
         assertEquals(Eligibility.MET, patient.getEligibility(Criterion.ADVANCE_CAD));
+        assertEquals(Eligibility.NOT_MET, patient.getEligibility(Criterion.ALCOHOL_ABUSE));
+        assertEquals(Eligibility.MET, patient.getEligibility(Criterion.ASP_FOR_MI));
+        assertEquals(Eligibility.NOT_MET, patient.getEligibility(Criterion.CREATININE));
+        assertEquals(Eligibility.MET, patient.getEligibility(Criterion.DIETSUPP_2MOS));
+        assertEquals(Eligibility.NOT_MET, patient.getEligibility(Criterion.DRUG_ABUSE));
+        assertEquals(Eligibility.MET, patient.getEligibility(Criterion.ENGLISH));
+        assertEquals(Eligibility.MET, patient.getEligibility(Criterion.HBA1C));
+        assertEquals(Eligibility.NOT_MET, patient.getEligibility(Criterion.KETO_1YR));
+        assertEquals(Eligibility.MET, patient.getEligibility(Criterion.MAJOR_DIABETES));
+        assertEquals(Eligibility.MET, patient.getEligibility(Criterion.MAKES_DECISIONS));
+        assertEquals(Eligibility.MET, patient.getEligibility(Criterion.MI_6MOS));
     }
 }
