487dd02e0f3614399bfe2193f4be32d907d63dbb
Michel Oleynik <michelole@gmail.com>
2018-03-16 14:31:49 +0100

    Organize imports

--- src/main/java/at/medunigraz/imi/bst/n2c2/evaluator/InterAnnotatorAgreement.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/evaluator/InterAnnotatorAgreement.java
@@ -1,13 +1,12 @@
 package at.medunigraz.imi.bst.n2c2.evaluator;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 
 public class InterAnnotatorAgreement extends AbstractEvaluator {
