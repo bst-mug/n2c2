73dca90838b567a9466fc9d2430c6d1ab722526f
Michel Oleynik <michelole@gmail.com>
2018-03-21 19:41:14 +0100

    Add XMLStatsWriter
    Writes stats into a XML file that can be consumed by e.g. Jenkins.

---
+++ src/main/java/at/medunigraz/imi/bst/n2c2/stats/XMLStatsWriter.java
@@ -0,0 +1,85 @@
+package at.medunigraz.imi.bst.n2c2.stats;
+
+import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class XMLStatsWriter extends AbstractStatsWriter {
+
+    private Document doc;
+    private OutputStream output;
+    private Element rootElement;
+
+    public XMLStatsWriter(OutputStream output) {
+        super(output);
+        this.output = output;
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+        DocumentBuilder documentBuilder = null;
+        try {
+            documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        doc = documentBuilder.newDocument();
+
+        writeHeader();
+    }
+
+    protected void writeHeader() {
+        rootElement = doc.createElement("report");
+        doc.appendChild(rootElement);
+        flush();
+    }
+
+    @Override
+    public void write(Criterion c, Float accuracy) {
+        Element topicElement = doc.createElement("criterion");
+        topicElement.setAttribute("name", c.name());
+        rootElement.appendChild(topicElement);
+
+        Element metricElement = doc.createElement("Accuracy");
+        metricElement.appendChild(doc.createTextNode(String.valueOf(accuracy)));
+
+        topicElement.appendChild(metricElement);
+    }
+
+    @Override
+    public void close() throws IOException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+        Transformer transformer = null;
+        try {
+            transformer = transformerFactory.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(output);
+
+        try {
+            transformer.transform(source, result);
+        } catch (TransformerException e) {
+            throw new IOException(e);
+        }
+    }
+
+    @Override
+    public void flush() {
+
+    }
+}
