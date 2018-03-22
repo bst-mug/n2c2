9ba94a9dc00ed6d3864284141ebed1c39b6bf6ef
Michel Oleynik <michelole@gmail.com>
2018-03-22 20:55:57 +0100

    Add integration tests
    The tests ensure that XML files generated by PatientDAO can be read and parsed by the iaa.py and generate expected results.

---
+++ src/test/java/at/medunigraz/imi/bst/n2c2/integration/DAOEvaluatorIntegrationTest.java
@@ -0,0 +1,65 @@
+package at.medunigraz.imi.bst.n2c2.integration;
+
+import at.medunigraz.imi.bst.n2c2.dao.PatientDAO;
+import at.medunigraz.imi.bst.n2c2.evaluator.InterAnnotatorAgreement;
+import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import at.medunigraz.imi.bst.n2c2.model.Eligibility;
+import at.medunigraz.imi.bst.n2c2.model.Patient;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.TemporaryFolder;
+import org.xml.sax.SAXException;
+
+import java.io.File;
+import java.io.IOException;
+
+import static junit.framework.Assert.assertEquals;
+
+@Category(IntegrationTest.class)
+public class DAOEvaluatorIntegrationTest {
+
+    private static final String GOLD = "/gold-standard/";
+
+    @Rule
+    public final TemporaryFolder testFolder = new TemporaryFolder();
+
+    private final File goldStandardFolder = new File(getClass().getResource(GOLD).getFile());
+    private final File sampleFile = new File(goldStandardFolder, "sample.xml");
+
+    @Before
+    public void SetUp() {
+        Assume.assumeTrue(InterAnnotatorAgreement.scriptExists());
+    }
+
+    @Test
+    public void regeneratedSampleFile() throws IOException, SAXException {
+        final File equalResultsFolder = testFolder.newFolder();
+        final File regeneratedSampleFile = new File(equalResultsFolder, "sample.xml");
+
+        // <ABDOMINAL met="not met" />
+        Patient patient = PatientDAO.fromXML(sampleFile);
+        PatientDAO.toXML(patient, regeneratedSampleFile);
+
+        InterAnnotatorAgreement iaa = new InterAnnotatorAgreement(goldStandardFolder, equalResultsFolder);
+        assertEquals(1, iaa.getF1(), 0.00001);
+    }
+
+    @Test
+    public void changedSampleFile() throws IOException, SAXException {
+        final File differentResultsFolder = testFolder.newFolder();
+        final File differentSampleFile = new File(differentResultsFolder, "sample.xml");
+
+        Patient patient = PatientDAO.fromXML(sampleFile);
+        patient.withCriterion(Criterion.ABDOMINAL, Eligibility.MET);
+        PatientDAO.toXML(patient, differentSampleFile);
+
+        InterAnnotatorAgreement iaa = new InterAnnotatorAgreement(goldStandardFolder, differentResultsFolder);
+        assertEquals(0, iaa.getF1(), 0.00001);
+
+        // TODO change after second version of iaa.py is released
+        // assertEquals(0.9230769, iaa.getF1(), 0.00001);  // 1 - 1/13 = 0.9230769
+    }
+}
