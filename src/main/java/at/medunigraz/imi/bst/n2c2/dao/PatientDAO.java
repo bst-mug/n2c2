ce34df3ff65c65f784d0f36105f2e0a5845aa9b9
Michel Oleynik <michelole@gmail.com>
2018-03-22 20:53:33 +0100

    Add methods to write a Patient back to XML (fixes #8)

--- src/main/java/at/medunigraz/imi/bst/n2c2/dao/PatientDAO.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/dao/PatientDAO.java
@@ -1,5 +1,7 @@
 package at.medunigraz.imi.bst.n2c2.dao;
 
+import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import at.medunigraz.imi.bst.n2c2.model.Eligibility;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 import at.medunigraz.imi.bst.n2c2.model.XMLTag;
 import org.w3c.dom.Document;
@@ -10,10 +12,10 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
 
 public class PatientDAO {
     public static Patient fromXML(File xmlFile) throws IOException, SAXException {
@@ -45,5 +47,75 @@ public class PatientDAO {
         }
 
         return patient;
+    }
+
+    public static void toXML(Patient patient, File outputFile) throws IOException {
+        toXML(patient, new FileOutputStream(outputFile));
+    }
+
+    public static OutputStream toXML(Patient patient, OutputStream outputStream) throws IOException {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+        DocumentBuilder documentBuilder = null;
+        try {
+            documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        Document doc = documentBuilder.newDocument();
+
+        Element rootElement = doc.createElement("PatientMatching");
+        doc.appendChild(rootElement);
+
+        Element textElement = doc.createElement("TEXT");
+        textElement.appendChild(doc.createCDATASection(patient.getText()));
+        rootElement.appendChild(textElement);
+
+        Element tagsElement = doc.createElement("TAGS");
+        rootElement.appendChild(tagsElement);
+
+        for (Criterion c : Criterion.values()) {
+            Eligibility e = patient.getEligibility(c);
+            if (e == null) {
+                continue;
+            }
+
+            Element criterionElement = doc.createElement(c.toString());
+            criterionElement.setAttribute("met", e.toString());
+            tagsElement.appendChild(criterionElement);
+        }
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+        Transformer transformer = null;
+        try {
+            transformer = transformerFactory.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        StreamResult result = new StreamResult(outputStream);
+
+        // Forces a newline after the header to make the output more similar to the sample file
+        // See https://stackoverflow.com/a/44323508
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+        Document header = documentBuilder.newDocument();
+        header.setXmlStandalone(true); // removes standalone="no" XML key
+        try {
+            transformer.transform(new DOMSource(header), result);
+        } catch (TransformerException e) {
+            throw new IOException(e);
+        }
+
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        try {
+            transformer.transform(new DOMSource(doc), result);
+        } catch (TransformerException e) {
+            throw new IOException(e);
+        }
+
+        return null;
     }
 }
