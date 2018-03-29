ed6fbc4e47713cce717ed352229e2784446c6180
Markus Kreuzthaler <markus.kreuzthaler@gmail.com>
2018-03-29 11:47:34 +0200

    Time series length calculation. dl4j pom update. CSVStatsWriteTest fix.

--- src/test/java/at/medunigraz/imi/bst/n2c2/stats/CSVStatsWriterTest.java
+++ src/test/java/at/medunigraz/imi/bst/n2c2/stats/CSVStatsWriterTest.java
@@ -1,32 +1,34 @@
 package at.medunigraz.imi.bst.n2c2.stats;
 
-import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.commons.io.FileUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
-import java.io.IOException;
-
-import static org.junit.Assert.assertEquals;
+import at.medunigraz.imi.bst.n2c2.model.Criterion;
 
 public class CSVStatsWriterTest {
-    @Rule
-    public final TemporaryFolder testFolder = new TemporaryFolder();
 
-    @Test
-    public void write() throws IOException {
-        final File actualFile = testFolder.newFile("test.csv");
-        final File expectedFile = new File(getClass().getResource("/stats/expected.csv").getFile());
+	@Rule
+	public final TemporaryFolder testFolder = new TemporaryFolder();
 
-        StatsWriter writer = new CSVStatsWriter(actualFile);
-        writer.write(Criterion.ABDOMINAL, 0.5f);
-        writer.close();
+	@Test
+	public void write() throws IOException {
+		final File actualFile = testFolder.newFile("test.csv");
+		final File expectedFile = new File(getClass().getResource("/stats/expected.csv").getFile());
 
-        String expected = FileUtils.readFileToString(expectedFile, "UTF-8");
-        String actual = FileUtils.readFileToString(actualFile, "UTF-8");
+		StatsWriter writer = new CSVStatsWriter(actualFile);
+		writer.write(Criterion.ABDOMINAL, 0.5f);
+		writer.close();
 
-        assertEquals(expected, actual);
-    }
+		String expected = FileUtils.readFileToString(expectedFile, "UTF-8").replaceAll("[\r\n]+", "");
+		String actual = FileUtils.readFileToString(actualFile, "UTF-8").replaceAll("[\r\n]+", "");
+
+		assertEquals(expected, actual);
+	}
 }
