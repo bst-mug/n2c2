31c75260833d3b271f5ca89178794955497b47f0
Michel Oleynik <michelole@gmail.com>
2018-03-29 11:01:50 +0200

    Add syntactic sugar for Classifier interface

--- src/main/java/at/medunigraz/imi/bst/n2c2/classifier/Classifier.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/classifier/Classifier.java
@@ -4,10 +4,16 @@ import at.medunigraz.imi.bst.n2c2.model.Eligibility;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public interface Classifier {
 
     void train(List<Patient> examples);
 
     Eligibility predict(Patient p);
+
+    default Map<Patient, Eligibility> predict(List<Patient> patientList) {
+        return patientList.stream().collect(Collectors.toMap(p -> p, p -> predict(p)));
+    }
 }
--- src/test/java/at/medunigraz/imi/bst/n2c2/classifier/MajorityClassifierTest.java
+++ src/test/java/at/medunigraz/imi/bst/n2c2/classifier/MajorityClassifierTest.java
@@ -6,7 +6,9 @@ import at.medunigraz.imi.bst.n2c2.model.Patient;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,16 +16,31 @@ public class MajorityClassifierTest {
 
     @Test
     public void trainPredict() {
-        List<Patient> examples = new ArrayList<>();
+        List<Patient> train = new ArrayList<>();
 
         // 2/3 not met is the expected output
-        examples.add(new Patient().withCriterion(Criterion.ABDOMINAL, Eligibility.MET));
-        examples.add(new Patient().withCriterion(Criterion.ABDOMINAL, Eligibility.NOT_MET));
-        examples.add(new Patient().withCriterion(Criterion.ABDOMINAL, Eligibility.NOT_MET));
+        train.add(new Patient().withCriterion(Criterion.ABDOMINAL, Eligibility.MET));
+        train.add(new Patient().withCriterion(Criterion.ABDOMINAL, Eligibility.NOT_MET));
+        train.add(new Patient().withCriterion(Criterion.ABDOMINAL, Eligibility.NOT_MET));
 
         MajorityClassifier mc = new MajorityClassifier(Criterion.ABDOMINAL);
-        mc.train(examples);
+        mc.train(train);
 
         assertEquals(Eligibility.NOT_MET, mc.predict(new Patient()));
+
+
+        List<Patient> test = new ArrayList<>();
+        Patient a = new Patient().withText("Patient A");
+        Patient b = new Patient().withText("Patient B");
+        test.add(a);
+        test.add(b);
+
+        Map<Patient, Eligibility> expected = new HashMap<>();
+        expected.put(a, Eligibility.NOT_MET);
+        expected.put(b, Eligibility.NOT_MET);
+
+        Map<Patient, Eligibility> actual = mc.predict(test);
+        assertEquals(expected, actual);
+
     }
 }
