7de86220b8ef5ecf9485fccecfe9e35a1397623b
Michel Oleynik <michelole@gmail.com>
2018-03-29 21:29:40 +0200

    Add CrossValidator (this fixes #12)

---
+++ src/main/java/at/medunigraz/imi/bst/n2c2/validation/CrossValidator.java
@@ -0,0 +1,72 @@
+package at.medunigraz.imi.bst.n2c2.validation;
+
+import at.medunigraz.imi.bst.n2c2.classifier.Classifier;
+import at.medunigraz.imi.bst.n2c2.classifier.factory.ClassifierFactory;
+import at.medunigraz.imi.bst.n2c2.evaluator.Evaluator;
+import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import at.medunigraz.imi.bst.n2c2.model.Patient;
+import at.medunigraz.imi.bst.n2c2.util.Dataset;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CrossValidator {
+
+    private static final Logger LOG = LogManager.getLogger();
+
+    private List<Patient> patients;
+    private ClassifierFactory classifierFactory;
+    private Evaluator evaluator;
+
+    CrossValidator(List<Patient> patients, ClassifierFactory classifierFactory, Evaluator evaluator) {
+        this.patients = patients;
+        this.classifierFactory = classifierFactory;
+        this.evaluator = evaluator;
+    }
+
+    public Map<Criterion, Double> evaluate() {
+        return evaluate(Dataset.DEFAULT_FOLDS);
+    }
+
+    public Map<Criterion, Double> evaluate(int k) {
+        List<Map<Criterion, Double>> metrics = new ArrayList<>();
+
+        Dataset dataset = new Dataset(patients);
+        dataset.splitIntoFolds(k);
+
+        for (int i = 0; i < k; i++) {
+            List<Patient> train = dataset.getTrainingSet(i);
+            List<Patient> test = dataset.getTestSet(i);
+            List<Patient> gold = dataset.getGoldSet(i);
+
+            metrics.add(evaluateFold(train, test, gold));
+        }
+
+        LOG.debug(metrics);
+
+        return metrics.stream().flatMap(e -> e.entrySet().stream()).collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.averagingDouble(Map.Entry::getValue)));
+    }
+
+    private Map<Criterion, Double> evaluateFold(List<Patient> train, List<Patient> test, List<Patient> gold) {
+        Map<Criterion, Double> ret = new HashMap<>();
+
+        for (Criterion c : Criterion.values()) {
+            Classifier classifier = classifierFactory.getClassifier(c);
+
+            classifier.train(train);
+            List<Patient> predicted = classifier.predict(test);
+
+            // TODO evaluate can be out of the loop if we use the same predicted list
+            evaluator.evaluate(gold, predicted);
+
+            ret.put(c, evaluator.getF1ByCriterion(c));
+        }
+
+        return ret;
+    }
+}
