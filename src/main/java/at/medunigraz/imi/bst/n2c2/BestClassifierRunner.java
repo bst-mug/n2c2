d22fbdfcd0277bccdbb4aca82bd36a2f8ed5a5e9
Michel Oleynik <michelole@gmail.com>
2018-04-03 12:47:03 +0200

    Add main class that runs the best classifier known so far (fixes #26)

--- src/main/java/at/medunigraz/imi/bst/n2c2/BestClassifierRunner.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/BestClassifierRunner.java
@@ -1,7 +1,41 @@
 package at.medunigraz.imi.bst.n2c2;
 
+import at.medunigraz.imi.bst.n2c2.classifier.factory.BestClassifierFactory;
+import at.medunigraz.imi.bst.n2c2.classifier.factory.ClassifierFactory;
+import at.medunigraz.imi.bst.n2c2.evaluator.Evaluator;
+import at.medunigraz.imi.bst.n2c2.evaluator.InterAnnotatorAgreement;
+import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import at.medunigraz.imi.bst.n2c2.model.Patient;
+import at.medunigraz.imi.bst.n2c2.stats.StatsWriter;
+import at.medunigraz.imi.bst.n2c2.stats.XMLStatsWriter;
+import at.medunigraz.imi.bst.n2c2.util.DatasetUtil;
+import at.medunigraz.imi.bst.n2c2.validation.CrossValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public class BestClassifierRunner {
-    public static void main(String[] args) {
-        
+
+    private static final Logger LOG = LogManager.getLogger();
+
+    public static void main(String[] args) throws IOException {
+        final File dataFolder = new File("data");
+        final File statsFile = new File("stats/best.xml");
+
+        List<Patient> patients = DatasetUtil.loadFromFolder(dataFolder);
+        ClassifierFactory factory = new BestClassifierFactory();
+        Evaluator evaluator = new InterAnnotatorAgreement();
+
+        CrossValidator cv = new CrossValidator(patients, factory, evaluator);
+        Map<Criterion, Double> metrics = cv.evaluate();
+        LOG.info(metrics);
+
+        StatsWriter writer = new XMLStatsWriter(statsFile);
+        writer.write(metrics);
+        writer.close();
     }
 }
