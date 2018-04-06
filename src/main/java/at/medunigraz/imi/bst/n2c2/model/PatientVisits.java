dee492d7fbd20c6cfa8a6d730b9723b3eaa82a56
Amila Kugic <amila.kugic@gmail.com>
2018-04-06 16:50:29 +0200

    #20

---
+++ src/main/java/at/medunigraz/imi/bst/n2c2/model/PatientVisits.java
@@ -0,0 +1,40 @@
+package at.medunigraz.imi.bst.n2c2.model;
+
+import java.util.Date;
+
+public class PatientVisits {
+
+	
+	private Integer visit_number = null; 
+	
+	private Date visit_date = null; 
+	
+	private String visit_text = null;
+
+	
+	public Integer getVisit_number() {
+		return visit_number;
+	}
+
+	public void setVisit_number(Integer visit_number) {
+		this.visit_number = visit_number;
+	}
+
+	public Date getVisit_date() {
+		return visit_date;
+	}
+
+	public void setVisit_date(Date visit_date) {
+		this.visit_date = visit_date;
+	}
+
+	public String getVisit_text() {
+		return visit_text;
+	}
+
+	public void setVisit_text(String visit_text) {
+		this.visit_text = visit_text;
+	} 
+	
+	
+}
---
+++ src/test/java/at/medunigraz/imi/bst/n2c2/zoning/ZoningTest.java
@@ -0,0 +1,52 @@
+package at.medunigraz.imi.bst.n2c2.zoning;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import at.medunigraz.imi.bst.n2c2.dao.PatientDAO;
+import at.medunigraz.imi.bst.n2c2.model.Patient;
+import at.medunigraz.imi.bst.n2c2.model.PatientVisits;
+
+public class ZoningTest {
+
+	@Test 
+	public void getAllData() throws IOException, SAXException{
+		
+		File sample = new File(getClass().getResource("/gold-standard/sample.xml").getPath());
+		Patient pat = new PatientDAO().fromXML(sample);
+		
+		ArrayList<PatientVisits> a_pv = pat.getAllVisits(); 
+		int size = a_pv.size(); 
+		assertTrue(size == 3); 
+		
+//		PatientVisits pv0 = a_pv.get(0);
+//		assertNull(pv0.getVisit_text());
+//		assertNull(pv0.getVisit_date());
+		
+		PatientVisits pv0 = a_pv.get(0);
+		assertThat(pv0.getVisit_text(), containsString("FISHKILL"));
+		
+		PatientVisits pv1 = a_pv.get(1);
+		assertThat(pv1.getVisit_text(), containsString("HPI"));
+		
+		PatientVisits pv2 = a_pv.get(2);
+		assertThat(pv2.getVisit_text(), containsString("PHYSICAL EXAMINATION"));
+		
+		PatientVisits tpv_first = pat.getFirstVisit(); 
+		assertThat(tpv_first.getVisit_text(), containsString("FISHKILL"));
+		
+		PatientVisits tpv_last = pat.getLastVisit(); 
+		assertThat(tpv_last.getVisit_text(), containsString("PHYSICAL EXAMINATION"));
+		
+		
+	}
+	
+	
+}
