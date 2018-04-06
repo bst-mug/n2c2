b92a79c94803d2255e656fefb75d080880e9b54f
Amila Kugic <amila.kugic@gmail.com>
2018-04-06 16:45:36 +0200

    takes care of Zoning (#20)

--- src/main/java/at/medunigraz/imi/bst/n2c2/model/Patient.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/model/Patient.java
@@ -1,5 +1,11 @@
 package at.medunigraz.imi.bst.n2c2.model;
 
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,4 +41,113 @@ public class Patient {
     public Eligibility getEligibility(Criterion criterion) {
         return criteria.get(criterion);
     }
-}
+    
+    /**
+     * getAllVisits() returns all the visits of one patient as
+     * an ArrayList, which includes the parsed date of the visit, 
+     * the number of the visits (depending on their order in the 
+     * original patient file, and the text of the visit itself. 
+     * @return ArrayList of the PatientVisits
+     */
+    public ArrayList<PatientVisits> getAllVisits(){
+    	String docFulltext = getText(); 
+    	ArrayList<PatientVisits> a_pv = new ArrayList<>(); 
+    	String[] visits = docFulltext.split("Record date:"); 
+    	
+    	for(int i = 0; i<visits.length; i++){
+    		PatientVisits pv = new PatientVisits(); 
+    		
+    		pv.setVisit_number(i);
+    		String line = visits[i].trim(); 
+    		if(line == "") line = null; 
+    		
+			String s_date = getFirstToken(line); 
+			if(s_date != null){
+				pv.setVisit_date(convertFormatDate(s_date));
+	    		pv.setVisit_text(trimVisitText(line));
+	    		a_pv.add(pv);
+			}
+			
+    	} // End of for loop 
+    	
+    	return a_pv; 
+    } // End of getAllVisits() 
+    
+    private String getFirstToken(String line){
+    	int lineLength = line.length(); 
+    	String dLine = null; 
+    	if(lineLength > 2){
+    		dLine = line.substring(0,12).trim(); 
+    	}
+    	return dLine; 
+    } // End of getFirstToken() 
+    
+    private Date convertFormatDate(String str_date) {
+    	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); 
+    	Date date = null;
+		try {
+			if(str_date != null){
+				date = sdf.parse(str_date);
+			}
+		} catch (ParseException | NullPointerException ne) {
+			date = null; 
+			ne.printStackTrace();
+		}
+    	return date; 
+    } // End of convertFormatDate() 
+    
+    private String trimVisitText(String visit_text){
+    	
+    	String end_id = "*******************************************"
+    			+ "**************************************************"
+    			+ "*******"; 
+    	if(visit_text.contains(end_id)){
+    		visit_text = visit_text.replace(end_id, ""); 
+    	}
+    	
+    	return visit_text; 
+    } // End of trimVisitText() 
+    
+    public PatientVisits getFirstVisit(){
+    	PatientVisits pv = new PatientVisits(); 
+    	if(getAllVisits().size()>0){
+    		pv = getAllVisits().get(0); 
+    	}
+    	return pv; 
+    } // End of getFirstVisit() 
+    
+    public PatientVisits getLastVisit(){
+    	PatientVisits pv = new PatientVisits();
+    	int pv_size = getAllVisits().size(); 
+    	if(pv_size>0){
+    		pv = getAllVisits().get(pv_size-1); 
+    	}
+    	return pv; 
+    } // End of getLastVisit()
+    
+    public ArrayList<PatientVisits> getMultipleVisits(int months){
+    	ArrayList<PatientVisits> a_pv_afterDate = new ArrayList<>(); 
+    	ArrayList<PatientVisits> a_pv = getAllVisits(); 
+    	Date date_ofLastVisit = getLastVisit().getVisit_date(); 
+    	
+    	Calendar c = Calendar.getInstance(); 
+    	c.setTime(date_ofLastVisit); 
+    	c.add(Calendar.MONTH, -months);
+    	Date date_inthepast = c.getTime(); 
+    	
+    	for(int i = 0; i<a_pv.size(); i++){
+    		if(a_pv.get(i).getVisit_date().after(date_inthepast)){
+    			a_pv_afterDate.add(a_pv_afterDate.get(i)); 
+    		}
+    	}
+    	
+    	return a_pv_afterDate; 
+    } // End of getMultipleVisits() 
+    
+    
+//  public Period getTimeIntervalBetweenVisits(PatientVisits visit1, PatientVisits visit2){
+//	// TODO 
+//} // End of getTimeIntervalBetweenVisits() 
+    
+    
+} // End of class Patient
