c6c352572042583c27746d6fecdb6c2f661a6f6d
Michel Oleynik <michelole@gmail.com>
2018-04-11 22:57:11 +0200

    Add support for new evaluation script (fixes #29)
    Update tests accordingly.

--- src/main/java/at/medunigraz/imi/bst/n2c2/evaluator/OfficialEvaluator.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/evaluator/OfficialEvaluator.java
@@ -1,6 +1,8 @@
 package at.medunigraz.imi.bst.n2c2.evaluator;
 
 import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import at.medunigraz.imi.bst.n2c2.model.Eligibility;
+import at.medunigraz.imi.bst.n2c2.model.MetricSet;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 import at.medunigraz.imi.bst.n2c2.util.DatasetUtil;
 import org.apache.logging.log4j.LogManager;
@@ -10,8 +12,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 
 
@@ -19,7 +19,9 @@ public class OfficialEvaluator implements Evaluator {
 
     private static final Logger LOG = LogManager.getLogger();
 
-    private static final String IAA_SCRIPT = "target/lib/iaa.py";
+    private static final int NUM_OUTPUT_FIELDS = 10;
+
+    private static final String IAA_SCRIPT = "target/track1_eval.py";
 
     /**
      * python iaa.py -t # folder1/ folder2/
@@ -32,13 +34,11 @@ public class OfficialEvaluator implements Evaluator {
     static {
         COMMAND.add("python");
         COMMAND.add(IAA_SCRIPT);
-        COMMAND.add("-t");
-        COMMAND.add("1");
     }
 
     private File goldStandard, results;
 
-    private Map<Criterion, Float> accuracyPerCriterion = new TreeMap<>();
+    private MetricSet metrics = new MetricSet();
 
     public OfficialEvaluator() throws FileNotFoundException {
         if (!scriptExists()) {
@@ -112,7 +112,7 @@ public class OfficialEvaluator implements Evaluator {
 
     @Override
     public double getOfficialRankingMeasure() {
-        return getOfficialRankingMeasureByCriterion(Criterion.OVERALL);
+        return metrics.getOfficialRankingMeasure();
     }
 
     private File createDirAndSave(List<Patient> patients, String prefix) throws IOException {
@@ -137,43 +137,70 @@ public class OfficialEvaluator implements Evaluator {
 
     private void parseOutput(String[] output) {
         /* Example output:
-            ********* CRITERIA *********
-                                  Acc.
-                       Abdominal  0.0000
-                    Advanced-cad  1.0000
-                   Alcohol-abuse  1.0000
-                      Asp-for-mi  1.0000
-                      Creatinine  1.0000
-                   Dietsupp-2mos  1.0000
-                      Drug-abuse  1.0000
-                         English  1.0000
-                           Hba1c  1.0000
-                        Keto-1yr  1.0000
-                  Major-diabetes  1.0000
-                 Makes-decisions  1.0000
-                         Mi-6mos  1.0000
-                                  ------
-                         Overall  0.0000
-            ()
-                   1 files found
-         */
-        for (String s : output) {
-            LOG.debug(s);
-            String[] fields = s.trim().split("\\s+");
+        ******************************************* TRACK 1 ********************************************
+                              ------------ met -------------    ------ not met -------    -- overall ---
+                              Prec.   Rec.    Speci.  F(b=1)    Prec.   Rec.    F(b=1)    F(b=1)  AUC
+                   Abdominal  0.6167  0.6916  0.7459  0.6520    0.8036  0.7459  0.7736    0.7128  0.7187
+                Advanced-cad  0.8235  0.7412  0.7712  0.7802    0.6741  0.7712  0.7194    0.7498  0.7562
+               Alcohol-abuse  0.0946  0.7000  0.7590  0.1667    0.9860  0.7590  0.8577    0.5122  0.7295
+                  Asp-for-mi  0.9235  0.7870  0.7414  0.8498    0.4674  0.7414  0.5733    0.7115  0.7642
+                  Creatinine  0.6159  0.8019  0.7088  0.6967    0.8600  0.7088  0.7771    0.7369  0.7553
+               Dietsupp-2mos  0.7417  0.7517  0.7194  0.7467    0.7299  0.7194  0.7246    0.7357  0.7356
+                  Drug-abuse  0.1264  0.7333  0.7216  0.2157    0.9801  0.7216  0.8312    0.5235  0.7275
+                     English  0.9750  0.7358  0.7826  0.8387    0.2045  0.7826  0.3243    0.5815  0.7592
+                       Hba1c  0.6818  0.7353  0.8118  0.7075    0.8483  0.8118  0.8297    0.7686  0.7736
+                    Keto-1yr  0.0149  1.0000  0.7700  0.0294    1.0000  0.7700  0.8701    0.4497  0.8850
+              Major-diabetes  0.7222  0.7500  0.6591  0.7358    0.6905  0.6591  0.6744    0.7051  0.7045
+             Makes-decisions  0.9906  0.7617  0.8182  0.8612    0.1200  0.8182  0.2093    0.5353  0.7900
+                     Mi-6mos  0.2840  0.8846  0.7786  0.4299    0.9855  0.7786  0.8699    0.6499  0.8316
+                              ------------------------------    ----------------------    --------------
+             Overall (micro)  0.6952  0.7546  0.7493  0.7237    0.8012  0.7493  0.7744    0.7490  0.7520
+             Overall (macro)  0.5855  0.7749  0.7529  0.5931    0.7192  0.7529  0.6950    0.6440  0.7639
 
-            if (fields.length != 2) {
+                                                           288 files found
+         */
+
+        for (int i = 0; i < output.length; i++) {
+            String line = output[i];
+            LOG.debug(line);
+
+            // Matches two or more consecutive whitespaces
+            String[] fields = line.trim().split("\\s{2,}");
+
+            if (fields.length != NUM_OUTPUT_FIELDS) {
                 continue;
             }
 
-            Criterion criterion = Criterion.get(fields[0]);
-            Float accuracy = Float.parseFloat(fields[1]);
+            String criterionName = fields[0];
+            if (!Criterion.isValid(criterionName)) {
+                continue;
+            }
 
-            accuracyPerCriterion.put(criterion, accuracy);
+            Criterion criterion = Criterion.get(criterionName);
+
+            // MET
+            metrics.withPrecision(criterion, Eligibility.MET, Double.parseDouble(fields[1]));
+            metrics.withRecall(criterion, Eligibility.MET, Double.parseDouble(fields[2]));
+            metrics.withSpecificity(criterion, Eligibility.MET, Double.parseDouble(fields[3]));
+            metrics.withF1(criterion, Eligibility.MET, Double.parseDouble(fields[4]));
+
+            // NOT MET
+            metrics.withPrecision(criterion, Eligibility.NOT_MET, Double.parseDouble(fields[5]));
+            metrics.withRecall(criterion, Eligibility.NOT_MET, Double.parseDouble(fields[6]));
+            metrics.withF1(criterion, Eligibility.NOT_MET, Double.parseDouble(fields[7]));
+
+            // OVERALL
+            metrics.withF1(criterion, Eligibility.OVERALL, Double.parseDouble(fields[8]));
+            metrics.withAreaUnderCurve(criterion, Eligibility.OVERALL, Double.parseDouble(fields[9]));
         }
+    }
+
+    public MetricSet getMetrics() {
+        return metrics;
     }
 
     @Override
     public double getOfficialRankingMeasureByCriterion(Criterion c) {
-        return accuracyPerCriterion.getOrDefault(c, 0f);
+        return metrics.getOfficialRankingMeasureByCriterion(c);
     }
 }
