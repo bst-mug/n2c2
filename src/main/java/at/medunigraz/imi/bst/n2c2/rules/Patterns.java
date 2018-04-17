376c5776eb7284b4fd49243db16ccd668dc8a364
Amila Kugic <amila.kugic@gmail.com>
2018-04-17 14:32:54 +0200

    added Patterns.java (#21) for better overview, still todos for that open

---
+++ src/main/java/at/medunigraz/imi/bst/n2c2/rules/Patterns.java
@@ -0,0 +1,64 @@
+package at.medunigraz.imi.bst.n2c2.rules;
+
+public class Patterns {
+	
+	final String r_1_digit = "[0-9]"; 
+	
+	final String r_2_digit = "[0-9][0-9]"; 
+	
+	final String r_whitespace = "\\s"; 
+	
+	final String r_forwardslash = "\\/"; 
+	
+	final String r_word = "(\\w+)"; 
+	
+	final String r_white_word_white = r_whitespace + r_word + r_whitespace;
+	
+	final String r_word_white = r_word + r_whitespace; 
+	
+	final String r_dot = "."; 
+	
+	final String r_criterionID = "(criterionID)";
+
+	
+	public String getR_1_digit() {
+		return r_1_digit;
+	}
+
+	public String getR_2_digit() {
+		return r_2_digit;
+	}
+
+	public String getR_whitespace() {
+		return r_whitespace;
+	}
+
+	public String getR_forwardslash() {
+		return r_forwardslash;
+	}
+
+	public String getR_word() {
+		return r_word;
+	}
+
+	public String getR_white_word_white() {
+		return r_white_word_white;
+	}
+
+	public String getR_word_white() {
+		return r_word_white;
+	}
+
+	public String getR_dot() {
+		return r_dot;
+	}
+
+	public String getR_criterionID() {
+		return r_criterionID;
+	} 
+	
+	
+	
+	
+
+}
