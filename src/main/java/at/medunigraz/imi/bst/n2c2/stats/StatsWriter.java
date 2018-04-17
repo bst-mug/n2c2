60037d90eba45ab90f051db80eea764eedd4b0c4
Michel Oleynik <michelole@gmail.com>
2018-04-17 16:37:17 +0200

    Add new default method for writing from a Metrics object

--- src/main/java/at/medunigraz/imi/bst/n2c2/stats/StatsWriter.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/stats/StatsWriter.java
@@ -1,16 +1,24 @@
 package at.medunigraz.imi.bst.n2c2.stats;
 
 import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import at.medunigraz.imi.bst.n2c2.model.metrics.Metrics;
 
 import java.io.Closeable;
 import java.io.Flushable;
+import java.util.Arrays;
 import java.util.Map;
 
 public interface StatsWriter extends Closeable, Flushable {
 
+    @Deprecated
     void write(Criterion c, Double accuracy);
 
+    @Deprecated
     default void write(Map<Criterion, Double> accuracyByCriterion) {
         accuracyByCriterion.forEach((key, value) -> write(key, value));
+    }
+
+    default void write(Metrics metrics) {
+        Arrays.stream(Criterion.classifiableValues()).forEach(c -> write(c, metrics.getOfficialRankingMeasureByCriterion(c)));
     }
 }
