40b76154517b6c2df551889c8f54688a926928bd
Michel Oleynik <michelole@gmail.com>
2018-04-20 17:35:38 +0200

    Introduce PatientBasedClassifier
    Add a base class for classifiers that operate on a patient level (and not on criterion level), like NN.

---
+++ src/main/java/at/medunigraz/imi/bst/n2c2/classifier/PatientBasedClassifier.java
@@ -0,0 +1,28 @@
+package at.medunigraz.imi.bst.n2c2.classifier;
+
+import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import at.medunigraz.imi.bst.n2c2.model.Eligibility;
+import at.medunigraz.imi.bst.n2c2.model.Patient;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class PatientBasedClassifier implements Classifier {
+
+    private List<Patient> predictedPatients = new ArrayList<>();
+
+    @Override
+    public List<Patient> predict(List<Patient> patientList) {
+        patientList.forEach(p -> Arrays.stream(Criterion.classifiableValues()).forEach(c -> p.withCriterion(c, this.predictIfNecessary(p, c))));
+        return patientList;
+    }
+
+    private Eligibility predictIfNecessary(Patient p, Criterion c) {
+        if (p.hasEligibility(c)) {
+            return p.getEligibility(c);
+        }
+
+        return predict(p, c);
+    }
+}
