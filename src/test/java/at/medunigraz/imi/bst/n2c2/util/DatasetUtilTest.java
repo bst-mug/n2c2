6dd937c292153b861529e6754ef555f1f9c471a0
Michel Oleynik <michelole@gmail.com>
2018-04-20 17:08:24 +0200

    Improve DatasetUtil and add tests
    For single-fold evaluation (#41), we need to be able to slice a list of patients by a given array of randomly selected indices. Therefore, add a getRandomIndices method to get a (deterministic) list of pseudo-random integers. Also, allow slice to be operated on a longer list, but restricted by initial index and length.

--- src/test/java/at/medunigraz/imi/bst/n2c2/util/DatasetUtilTest.java
+++ src/test/java/at/medunigraz/imi/bst/n2c2/util/DatasetUtilTest.java
@@ -8,8 +8,11 @@ import org.junit.rules.TemporaryFolder;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class DatasetUtilTest {
@@ -30,5 +33,73 @@ public class DatasetUtilTest {
 
         File expected = new File(output, filename);
         assertTrue(expected.exists());
+    }
+
+    @Test
+    public void slice() {
+        List<Patient> patients = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            patients.add(new Patient().withID(String.valueOf(i)));
+        }
+
+        int[] indices = new int[]{0, 3, 4};
+        List<Patient> slicedPatients = DatasetUtil.slice(patients, indices);
+        assertEquals(indices.length, slicedPatients.size());
+        assertTrue(slicedPatients.get(0).getID().equals("0"));
+        assertTrue(slicedPatients.get(1).getID().equals("3"));
+        assertTrue(slicedPatients.get(2).getID().equals("4"));
+    }
+
+    @Test
+    public void sliceWithIndices() {
+        List<Patient> patients = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            patients.add(new Patient().withID(String.valueOf(i)));
+        }
+
+        int[] indices = new int[]{0, 3, 4};
+
+        List<Patient> slicedPatients = DatasetUtil.slice(patients, indices, 0, 3);
+        assertEquals(3, slicedPatients.size());
+        assertTrue(slicedPatients.get(0).getID().equals("0"));
+        assertTrue(slicedPatients.get(1).getID().equals("3"));
+        assertTrue(slicedPatients.get(2).getID().equals("4"));
+
+        slicedPatients = DatasetUtil.slice(patients, indices, 0, 2);
+        assertEquals(2, slicedPatients.size());
+        assertTrue(slicedPatients.get(0).getID().equals("0"));
+        assertTrue(slicedPatients.get(1).getID().equals("3"));
+
+        slicedPatients = DatasetUtil.slice(patients, indices, 1, 1);
+        assertEquals(1, slicedPatients.size());
+        assertTrue(slicedPatients.get(0).getID().equals("3"));
+
+        // Length longer than indices
+        slicedPatients = DatasetUtil.slice(patients, indices, 2, 2);
+        assertEquals(1, slicedPatients.size());
+        assertTrue(slicedPatients.get(0).getID().equals("4"));
+    }
+
+    @Test
+    public void getRandomIndices() {
+        final int SIZE = 5;
+        final int MAX = 5;
+        int[] chosen = DatasetUtil.getRandomIndices(SIZE, MAX);
+
+        assertEquals(SIZE, chosen.length);
+
+        // Indices should not repeat.
+        // We create and set and check the length is the same.
+        // We later use the set to further tests.
+        Set<Integer> chosenSet = new HashSet<>();
+        for (int i : chosen) {
+            chosenSet.add(chosen[i]);
+        }
+        assertEquals(SIZE, chosenSet.size());
+
+        // Elements should be smaller than MAX
+        for (int i : chosen) {
+            assertTrue(chosen[i] < MAX);
+        }
     }
 }
