ab0a79af3b4551330fd017946afa0d764ce4a7af
Markus Kreuzthaler <markus.kreuzthaler@gmail.com>
2018-04-23 17:19:59 +0200

    Integration with 10-fold cross validation (BILSTMC3G)

--- src/main/java/at/medunigraz/imi/bst/n2c2/NNBestClassifierRunner.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/NNBestClassifierRunner.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import at.medunigraz.imi.bst.n2c2.classifier.factory.ClassifierFactory;
-import at.medunigraz.imi.bst.n2c2.classifier.factory.SVMClassifierFactory;
+import at.medunigraz.imi.bst.n2c2.classifier.factory.NNClassifierFactory;
 import at.medunigraz.imi.bst.n2c2.evaluator.OfficialEvaluator;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 import at.medunigraz.imi.bst.n2c2.model.metrics.MetricSet;
@@ -21,27 +21,27 @@ import at.medunigraz.imi.bst.n2c2.validation.NNCrossValidator;
 
 public class NNBestClassifierRunner {
 
-    private static final Logger LOG = LogManager.getLogger();
+	private static final Logger LOG = LogManager.getLogger();
 
-    public static void main(String[] args) throws IOException {
-        final File dataFolder = new File("data");
-        final File xmlStatsFile = new File("stats/best.xml");
-        final File csvStatsFile = new File("stats/best.csv");
+	public static void main(String[] args) throws IOException {
+		final File dataFolder = new File("Z:/n2c2/data/samplesTraining");
+		final File xmlStatsFile = new File("stats/best.xml");
+		final File csvStatsFile = new File("stats/best.csv");
 
-        List<Patient> patients = DatasetUtil.loadFromFolder(dataFolder);
-        ClassifierFactory factory = new SVMClassifierFactory();
-        OfficialEvaluator evaluator = new OfficialEvaluator();
+		List<Patient> patients = DatasetUtil.loadFromFolder(dataFolder);
+		ClassifierFactory factory = new NNClassifierFactory();
+		OfficialEvaluator evaluator = new OfficialEvaluator();
 
-        CrossValidator cv = new NNCrossValidator(patients, factory, evaluator);
-        MetricSet metrics = (MetricSet) cv.validate();
-        LOG.info(metrics);
+		CrossValidator cv = new NNCrossValidator(patients, factory, evaluator);
+		MetricSet metrics = (MetricSet) cv.validate();
+		LOG.info(metrics);
 
-        StatsWriter xmlWriter = new XMLStatsWriter(xmlStatsFile);
-        xmlWriter.write(metrics);
-        xmlWriter.close();
+		StatsWriter xmlWriter = new XMLStatsWriter(xmlStatsFile);
+		xmlWriter.write(metrics);
+		xmlWriter.close();
 
-        StatsWriter csvWriter = new CSVStatsWriter(csvStatsFile);
-        csvWriter.write(metrics);
-        csvWriter.close();
-    }
+		StatsWriter csvWriter = new CSVStatsWriter(csvStatsFile);
+		csvWriter.write(metrics);
+		csvWriter.close();
+	}
 }
