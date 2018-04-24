ef63a04f1616b8d2e61b248a752cc6ce1acf57b7
Michel Oleynik <michelole@gmail.com>
2018-04-24 22:13:54 +0200

    Predict at least the majority for imbalanced classes
    Such as ALCOHOL-ABUSE, ENGLISH, KETO-1YR, and MAKES-DECISIONS.
    
    So that we can have a baseline run using the official metrics.

--- src/main/java/at/medunigraz/imi/bst/n2c2/rules/RuleBasedClassifier.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/rules/RuleBasedClassifier.java
@@ -15,18 +15,21 @@ public class RuleBasedClassifier extends CriterionBasedClassifier {
 	
 
 	
-	private static Map<Criterion, Classifiable> name = new HashMap<>();
+	private static final Map<Criterion, Classifiable> name = new HashMap<>();
 	static {
-		name.put(Criterion.HBA1C, new HbA1c());
         name.put(Criterion.ABDOMINAL, new Abdominal());
         name.put(Criterion.ADVANCED_CAD, new AdvancedCAD());
+        name.put(Criterion.ALCOHOL_ABUSE, new AlcoholAbuse());
         name.put(Criterion.ASP_FOR_MI, new AspForMi());
         name.put(Criterion.CREATININE, new Creatinine());
         name.put(Criterion.DIETSUPP_2MOS, new Dietsupp2mos());
         name.put(Criterion.DRUG_ABUSE, new DrugAbuse());
+        name.put(Criterion.ENGLISH, new English());
+        name.put(Criterion.HBA1C, new HbA1c());
+        name.put(Criterion.KETO_1YR, new Keto1Yr());
         name.put(Criterion.MAJOR_DIABETES, new MajorDiabetes());
+        name.put(Criterion.MAKES_DECISIONS, new MakesDecisions());
         name.put(Criterion.MI_6MOS, new MI6Mos());
-		// FIXME 
 	}
 	
 	Rules r = new Rules(); 
--- src/test/java/at/medunigraz/imi/bst/n2c2/classifier/rules/RuleBasedClassifierTest.java
+++ src/test/java/at/medunigraz/imi/bst/n2c2/classifier/rules/RuleBasedClassifierTest.java
@@ -42,16 +42,16 @@ public class RuleBasedClassifierTest {
 
         assertEquals(Eligibility.NOT_MET, (new RuleBasedClassifier(Criterion.ABDOMINAL).predict(p)));
         assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.ADVANCED_CAD).predict(p)));
-//        assertEquals(Eligibility.NOT_MET, (new RuleBasedClassifier(Criterion.ALCOHOL_ABUSE).predict(p)));
+        assertEquals(Eligibility.NOT_MET, (new RuleBasedClassifier(Criterion.ALCOHOL_ABUSE).predict(p)));
         assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.ASP_FOR_MI).predict(p)));
         assertEquals(Eligibility.NOT_MET, (new RuleBasedClassifier(Criterion.CREATININE).predict(p)));
         assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.DIETSUPP_2MOS).predict(p)));
         assertEquals(Eligibility.NOT_MET, (new RuleBasedClassifier(Criterion.DRUG_ABUSE).predict(p)));
-//        assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.ENGLISH).predict(p)));
+        assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.ENGLISH).predict(p)));
 //        assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.HBA1C).predict(p)));
-//        assertEquals(Eligibility.NOT_MET, (new RuleBasedClassifier(Criterion.KETO_1YR).predict(p)));
+        assertEquals(Eligibility.NOT_MET, (new RuleBasedClassifier(Criterion.KETO_1YR).predict(p)));
         assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.MAJOR_DIABETES).predict(p)));
-//        assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.MAKES_DECISIONS).predict(p)));
+        assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.MAKES_DECISIONS).predict(p)));
         assertEquals(Eligibility.MET, (new RuleBasedClassifier(Criterion.MI_6MOS).predict(p)));
     }
 	
