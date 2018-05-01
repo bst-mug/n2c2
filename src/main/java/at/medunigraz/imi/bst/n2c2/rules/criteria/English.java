66505a8bae38e2474b331da839c5574e9798edeb
Michel Oleynik <michelole@gmail.com>
2018-05-01 17:16:19 +0200

    Add some naive patterns for `ENGLISH` (refs #21)
    This increases accuracy by 4%.

--- src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/English.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/English.java
@@ -3,10 +3,22 @@ package at.medunigraz.imi.bst.n2c2.rules.criteria;
 import at.medunigraz.imi.bst.n2c2.model.Eligibility;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class English extends BaseClassifiable {
+
+    private static final List<Pattern> NEGATIVE_MARKERS = new ArrayList<>();
+
+    static {
+        NEGATIVE_MARKERS.add(Pattern.compile("spanish", Pattern.CASE_INSENSITIVE)); // 117.xml: Spanish-speaking
+        //NEGATIVE_MARKERS.add(Pattern.compile("interpreter", Pattern.CASE_INSENSITIVE)); // 242.xml: conducted with interpreter present
+        NEGATIVE_MARKERS.add(Pattern.compile("translated", Pattern.CASE_INSENSITIVE));// 371.xml: South Korea who has been in Canada for one year, translated by
+    }
 
     @Override
     public Eligibility isMet(Patient p) {
-        return Eligibility.MET;
+        return findAnyPattern(p.getText(), NEGATIVE_MARKERS) ? Eligibility.NOT_MET : Eligibility.MET;
     }
 }
