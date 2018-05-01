ac61be833af686ab0d64c75d7d003d29f4d123d7
Michel Oleynik <michelole@gmail.com>
2018-05-01 12:58:04 +0200

    Implement basic regex for `CREATININE` (refs #21)
    This leads to a 16% increase in accuracy for this criterion.

--- src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/Creatinine.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/Creatinine.java
@@ -3,12 +3,34 @@ package at.medunigraz.imi.bst.n2c2.rules.criteria;
 import at.medunigraz.imi.bst.n2c2.model.Eligibility;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 
-public class Creatinine implements Classifiable {
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Creatinine extends BaseClassifiable {
+
+    //final public String[] a_CriterionID_creatinine = {"Creatinine", "crea", "cre", "Cr", "CRE", "CREA"};
+
+    /**
+     * According to the provided annotations, 1.1 is still NOT_MET
+     */
+    private static final double MAX_VALUE = 1.1;
+
+    // TODO check whether creatinine > 10 is possible
+    // 107.xml: creatinine of  1.69
+    // 105.xml: Cr 1.4
+    // 100.xml: Creatinine             1.0
+    private static final Pattern REGEX = Pattern.compile("(?:cr|creatinine) ([0-9]\\.[0-9])", Pattern.CASE_INSENSITIVE);
 
     @Override
     public Eligibility isMet(Patient p) {
-        return Eligibility.NOT_MET;
-        // TODO Auto-generated method stub
+        List<Double> values = findAllValues(p.getText(), REGEX);
 
+        for (Double value : values) {
+            if (value > MAX_VALUE) {
+                return Eligibility.MET;
+            }
+        }
+
+        return Eligibility.NOT_MET;
     }
 }
