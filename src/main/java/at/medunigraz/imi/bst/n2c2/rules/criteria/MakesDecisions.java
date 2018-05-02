6fec4028f4a8b9f4c60d0c93c434092dc737d92a
Amila Kugic <amila.kugic@gmail.com>
2018-05-02 17:53:54 +0200

    #21 - prep for changes in `MAKES-DECISION`-- TODO for tomorrow

--- src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/MakesDecisions.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/MakesDecisions.java
@@ -1,12 +1,26 @@
 package at.medunigraz.imi.bst.n2c2.rules.criteria;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import at.medunigraz.imi.bst.n2c2.model.Eligibility;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 
 public class MakesDecisions extends BaseClassifiable {
 
+	private static final List<Pattern> NEGATIVE_MARKERS = new ArrayList<>();
+
+    static {
+    	
+        NEGATIVE_MARKERS.add(Pattern.compile("", Pattern.CASE_INSENSITIVE)); // .xml: 
+        
+    }
+	
+	
     @Override
     public Eligibility isMet(Patient p) {
-        return Eligibility.MET;
+//        return Eligibility.MET;
+    	return findAnyPattern(p.getText(), NEGATIVE_MARKERS) ? Eligibility.NOT_MET : Eligibility.MET;
     }
 }
