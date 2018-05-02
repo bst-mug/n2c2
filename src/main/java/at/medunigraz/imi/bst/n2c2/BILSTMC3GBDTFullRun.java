9c850db151a09453e8f60acf7171fd868621fc7f
Michel Oleynik <michelole@gmail.com>
2018-05-02 19:19:20 +0200

    Move paths to config to avoid merge conflicts

--- src/main/java/at/medunigraz/imi/bst/n2c2/BILSTMC3GBDTFullRun.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/BILSTMC3GBDTFullRun.java
@@ -1,5 +1,16 @@
 package at.medunigraz.imi.bst.n2c2;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import at.medunigraz.imi.bst.n2c2.classifier.Classifier;
 import at.medunigraz.imi.bst.n2c2.classifier.factory.BDTClassifierFactory;
 import at.medunigraz.imi.bst.n2c2.classifier.factory.ClassifierFactory;
@@ -9,17 +20,11 @@ import at.medunigraz.imi.bst.n2c2.model.Criterion;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 import at.medunigraz.imi.bst.n2c2.nn.BILSTMC3GClassifier;
 import at.medunigraz.imi.bst.n2c2.util.DatasetUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
 
 /**
  * Full run of BILSTMC3G with boosted (AdaBoostM1) decision tree (RandomForest)
  * at the end.
- * 
+ *
  * @author Markus
  *
  */
@@ -30,19 +35,19 @@ public class BILSTMC3GBDTFullRun {
 	public static void main(String[] args) throws IOException {
 
 		// just needed for simulating test data for the moment
-        final File patienFolder = new File(Config.NN_SAMPLES_TRAINING);
+		final File patienFolder = new File(Config.NN_SAMPLES_TRAINING);
 
 		// path to persisted BILSTMC3G model
-        final String modelPath = Config.NN_MODELS;
+		final String modelPath = Config.NN_MODELS;
 
 		// path for training boosted decision tree (RandomForest)
-        final String pathTrainingBDT = Config.NN_SAMPLES_TRAINING_BDT;
+		final String pathTrainingBDT = Config.NN_SAMPLES_TRAINING_BDT;
 
 		// probabilities persistence of BILSTMC3G in patient XML format
-        final String pathTestBDT = Config.NN_SAMPLES_TEST_BDT;
+		final String pathTestBDT = Config.NN_SAMPLES_TEST_BDT;
 
 		// path for saving prediction on test patients
-        final String pathPredicted = Config.NN_SAMPLES_PREDICTED_BDT;
+		final String pathPredicted = Config.NN_SAMPLES_PREDICTED_BDT;
 
 		ClassifierFactory classifierFactory = new BDTClassifierFactory();
 
@@ -61,7 +66,7 @@ public class BILSTMC3GBDTFullRun {
 
 		// generate BILSTMC3G output
 		BILSTMC3GClassifier biLSTMC3GClassifier = new BILSTMC3GClassifier(modelPath);
-        patientsTestBILSTMC3G.forEach(p -> biLSTMC3GClassifier.predictAndOverwrite(p, pathTestBDT));    //1
+		patientsTestBILSTMC3G.forEach(p -> biLSTMC3GClassifier.predictAndOverwrite(p, pathTestBDT));    //1
 
 		// load prediction format
 		List<Patient> patientsTrainingBDT = DatasetUtil.loadFromFolder(new File(pathTrainingBDT));
@@ -73,8 +78,8 @@ public class BILSTMC3GBDTFullRun {
 			LOG.info("Training and prediction for criterion {}...", c);
 			Classifier classifier = classifierFactory.getClassifier(c);
 
-            classifier.train(patientsTrainingBDT);  //2
-            predicted = classifier.predict(patientsTestBDT);    //3
+			classifier.train(patientsTrainingBDT);  //2
+			predicted = classifier.predict(patientsTestBDT);    //3
 		}
 
 		// write back original text
