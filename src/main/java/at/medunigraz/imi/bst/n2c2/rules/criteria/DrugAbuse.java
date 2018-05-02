af5eb60fecf01f9a1304f68b8579daca4a8bbe22
Amila Kugic <amila.kugic@gmail.com>
2018-05-02 17:44:52 +0200

    #21 updated rules for 'DRUG-ABUSE' improvement from 94% to 96%

--- src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/DrugAbuse.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/DrugAbuse.java
@@ -1,12 +1,32 @@
 package at.medunigraz.imi.bst.n2c2.rules.criteria;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import at.medunigraz.imi.bst.n2c2.model.Eligibility;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 
 public class DrugAbuse extends BaseClassifiable {
 
+	private static final List<Pattern> POSITIVE_MARKERS = new ArrayList<>();
+
+    static {
+    	
+    	POSITIVE_MARKERS.add(Pattern.compile("cocaine", Pattern.CASE_INSENSITIVE)); // 356.xml: he has used cocaine and crack
+    	
+    	POSITIVE_MARKERS.add(Pattern.compile("marijuana", Pattern.CASE_INSENSITIVE)); // 382.xml: +occasional marijuana.
+    
+    	POSITIVE_MARKERS.add(Pattern.compile("IV drug user", Pattern.CASE_INSENSITIVE)); // 382.xml: H/O drug abuse : IV drug user stopped heroin
+    	
+    	
+    }
+	
+	
+	
     @Override
     public Eligibility isMet(Patient p) {
-        return Eligibility.NOT_MET;
+//        return Eligibility.NOT_MET;
+    	return findAnyPattern(p.getText(), POSITIVE_MARKERS) ? Eligibility.MET : Eligibility.NOT_MET;
     }
 }
