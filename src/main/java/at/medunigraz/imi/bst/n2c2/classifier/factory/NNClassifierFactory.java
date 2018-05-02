c91d186eda11787587e62184622675b5c68e0616
Michel Oleynik <michelole@gmail.com>
2018-05-02 14:15:33 +0200

    Make NNClassifierFactory use the final BILSTMC3GClassifier
    For internal consistency.

--- src/main/java/at/medunigraz/imi/bst/n2c2/classifier/factory/NNClassifierFactory.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/classifier/factory/NNClassifierFactory.java
@@ -2,11 +2,11 @@ package at.medunigraz.imi.bst.n2c2.classifier.factory;
 
 import at.medunigraz.imi.bst.n2c2.classifier.Classifier;
 import at.medunigraz.imi.bst.n2c2.model.Criterion;
-import at.medunigraz.imi.bst.n2c2.nn.LSTMClassifier;
+import at.medunigraz.imi.bst.n2c2.nn.BILSTMC3GClassifier;
 
 public class NNClassifierFactory implements ClassifierFactory {
 
-	private static final Classifier classifier = new LSTMClassifier();
+    private static final Classifier classifier = new BILSTMC3GClassifier();
 
 	@Override
 	public Classifier getClassifier(Criterion criterion) {
