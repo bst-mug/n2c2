3a31feae30d2e7eb656b92a270bd9eab5c792346
Amila Kugic <amila.kugic@gmail.com>
2018-05-04 11:12:44 +0200

    #21 - added RBC for `MAKES-DECISION`

--- src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/MakesDecisions.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/MakesDecisions.java
@@ -12,8 +12,11 @@ public class MakesDecisions extends BaseClassifiable {
 	private static final List<Pattern> NEGATIVE_MARKERS = new ArrayList<>();
 
     static {
-//        NEGATIVE_MARKERS.add(Pattern.compile("interactive", Pattern.CASE_INSENSITIVE)); // .xml: 
-//        NEGATIVE_MARKERS.add(Pattern.compile("aware", Pattern.CASE_INSENSITIVE)); // .xml: 
+//        NEGATIVE_MARKERS.add(Pattern.compile("altered mental", Pattern.CASE_INSENSITIVE)); // 308.xml: CHIEF COMPLAINT: Altered mental status 
+        NEGATIVE_MARKERS.add(Pattern.compile("severe dementia", Pattern.CASE_INSENSITIVE)); // 357.xml: 80 year old female with severe dementia and ESRD
+//        NEGATIVE_MARKERS.add(Pattern.compile("(Alzheimer|Alzheimers) (meds|medication)", Pattern.CASE_INSENSITIVE)); // 357.xml: Dementia - Cont Alzheimers meds#
+//        NEGATIVE_MARKERS.add(Pattern.compile("Alzheimer", Pattern.CASE_INSENSITIVE)); // 357.xml: Dementia - Cont Alzheimers meds#
+        NEGATIVE_MARKERS.add(Pattern.compile("(Alzheimer|Alzheimers)", Pattern.CASE_INSENSITIVE)); 
         
     }
 	
