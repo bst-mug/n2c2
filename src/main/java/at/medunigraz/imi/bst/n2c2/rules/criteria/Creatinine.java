5026c620869375561cf776830c88be391550a7da
Michel Oleynik <michelole@gmail.com>
2018-05-05 21:56:05 +0200

    Fine tune markers for `CREATININE` (refs #21)
    This increases accuracy by 3.5%.

--- src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/Creatinine.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/rules/criteria/Creatinine.java
@@ -3,6 +3,7 @@ package at.medunigraz.imi.bst.n2c2.rules.criteria;
 import at.medunigraz.imi.bst.n2c2.model.Eligibility;
 import at.medunigraz.imi.bst.n2c2.model.Patient;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -14,27 +15,40 @@ public class Creatinine extends BaseClassifiable {
     // 225.xml: 1.2 => NOT_MET
     // 272.xml: 1.4 => NOT_MET
     // 144.xml: 1.43 => MET
-    private static final double MAX_VALUE = 1.4;
+    private static final double MIN_VALUE = 1.4;
 
+    // Sanity check
+    private static final double MAX_VALUE = 10;
+
+    private static final List<Pattern> POSITIVE_MARKERS = new ArrayList<>();
     // TODO check whether creatinine > 10 is possible
-    // 313.xml: mcalb/cr 200
+    // 313.xml (MET): mcalb/cr 200
     // 250.xml: creatinine 2
     // 268.xml: CRE       1.6
     // 107.xml: creatinine of  1.69
     // 105.xml: Cr 1.4
     // 100.xml: Creatinine             1.0
-    private static final Pattern REGEX = Pattern.compile("(?:cr|creatinine|cre)(?: \\(Stat Lab\\))? +(?:of |is |stable at )?([0-9]\\.[0-9])", Pattern.CASE_INSENSITIVE);
+    // 313.xml (MET): elevated creatinine
+    private static final Pattern VALUE_REGEX = Pattern.compile("[^/](?:cr|creatinine|cre)[^a-z].{0,15}?([0-9]\\.[0-9](?:[0-9])?)", Pattern.CASE_INSENSITIVE);
+
+    static {
+        // 299.xml: rising serum creatinine
+        POSITIVE_MARKERS.add(Pattern.compile("(elevated|rising|high).{1,10}creatinine", Pattern.CASE_INSENSITIVE));
+
+        // 356.xml: creatinine has been high
+        POSITIVE_MARKERS.add(Pattern.compile("creatinine.{1,10}(elevated|rising|high)", Pattern.CASE_INSENSITIVE));
+    }
 
     @Override
     public Eligibility isMet(Patient p) {
-        List<Double> values = findAllValues(p.getCleanedText(), REGEX);
+        List<Double> values = findAllValues(p.getCleanedText(), VALUE_REGEX);
 
         for (Double value : values) {
-            if (value > MAX_VALUE) {
+            if (value > MIN_VALUE && value < MAX_VALUE) {
                 return Eligibility.MET;
             }
         }
 
-        return Eligibility.NOT_MET;
+        return findAnyPattern(p.getCleanedText(), POSITIVE_MARKERS) ? Eligibility.MET : Eligibility.NOT_MET;
     }
 }
