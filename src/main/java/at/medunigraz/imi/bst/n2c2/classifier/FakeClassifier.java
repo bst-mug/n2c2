1394b2f5069a9a5c9ebd9699ac03017183334f77
Michel Oleynik <michelole@gmail.com>
2018-12-10 19:34:46 +0100

    Fake Fact
    Allow `FakeClassifier` to also peek on test data and add a factory for it, so that it can be used side-by-side with other strategies.

--- src/main/java/at/medunigraz/imi/bst/n2c2/classifier/FakeClassifier.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/classifier/FakeClassifier.java
@@ -7,11 +7,16 @@ import at.medunigraz.imi.bst.n2c2.util.DatasetUtil;
 
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FakeClassifier extends CriterionBasedClassifier {
 
-    private static final File DATA_FOLDER = new File("data/train");
-    private static final List<Patient> PATIENTS = DatasetUtil.loadFromFolder(DATA_FOLDER);
+    private static final File TRAIN_FOLDER = new File("data/train");
+    private static final File TEST_FOLDER = new File("data/test");
+
+    private static final List<Patient> PATIENTS = Stream.concat(DatasetUtil.loadFromFolder(TRAIN_FOLDER).stream(),
+            DatasetUtil.loadFromFolder(TEST_FOLDER).stream()).collect(Collectors.toList());
 
     public FakeClassifier(Criterion c) {
         super(c);
