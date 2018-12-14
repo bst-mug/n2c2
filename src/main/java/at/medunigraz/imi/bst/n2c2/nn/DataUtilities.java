c5adf71c0908e1d4c7a0b2ec5b6af9604f479036
Michel Oleynik <michelole@gmail.com>
2018-12-14 11:44:13 +0100

    Remove unused `extractTarGz` method (refs #73)

--- src/main/java/at/medunigraz/imi/bst/n2c2/nn/DataUtilities.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/nn/DataUtilities.java
@@ -1,11 +1,6 @@
 package at.medunigraz.imi.bst.n2c2.nn;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -17,9 +12,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
-import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.io.IOUtils;
 import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.LowerCaseFilter;
@@ -38,9 +30,6 @@ import org.apache.lucene.util.AttributeFactory;
  *
  */
 public class DataUtilities {
-
-	// defining buffer size
-	private static final int BUFFER_SIZE = 4096;
 
 	private static final Pattern CLEANER_REGEX = Pattern.compile("\\p{javaWhitespace}+");
 
@@ -64,40 +53,6 @@ public class DataUtilities {
 			e.printStackTrace();
 		}
 
-	}
-
-	public static void extractTarGz(String filePath, String outputPath) throws IOException {
-		int fileCount = 0;
-		int dirCount = 0;
-		System.out.print("Extracting files");
-
-		try (TarArchiveInputStream tais = new TarArchiveInputStream(
-				new GzipCompressorInputStream(new BufferedInputStream(new FileInputStream(filePath))))) {
-			TarArchiveEntry entry;
-
-			while ((entry = (TarArchiveEntry) tais.getNextEntry()) != null) {
-
-				// create directories
-				if (entry.isDirectory()) {
-					new File(outputPath + entry.getName()).mkdirs();
-					dirCount++;
-				} else {
-					int count;
-					byte data[] = new byte[BUFFER_SIZE];
-
-					FileOutputStream fos = new FileOutputStream(outputPath + entry.getName());
-					BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER_SIZE);
-					while ((count = tais.read(data, 0, BUFFER_SIZE)) != -1) {
-						dest.write(data, 0, count);
-					}
-					dest.close();
-					fileCount++;
-				}
-				if (fileCount % 1000 == 0)
-					System.out.print(".");
-			}
-		}
-		System.out.println("\n" + fileCount + " files and " + dirCount + " directories extracted to: " + outputPath);
 	}
 
 	/**
