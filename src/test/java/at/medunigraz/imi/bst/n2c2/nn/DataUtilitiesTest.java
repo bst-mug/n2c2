160052b855a1156ff47b995dd37adb55d0982a47
Michel Oleynik <michelole@gmail.com>
2018-12-14 11:37:52 +0100

    Remove ignored test (refs #73)

--- src/test/java/at/medunigraz/imi/bst/n2c2/nn/DataUtilitiesTest.java
+++ src/test/java/at/medunigraz/imi/bst/n2c2/nn/DataUtilitiesTest.java
@@ -59,38 +59,4 @@ public class DataUtilitiesTest {
 		assertEquals(FileUtils.readFileToString(expectedTrigrams, "UTF-8"), textTrigrams.toString());
 	}
 
-	@Ignore
-	public void train() {
-
-		// read in patients
-		File sampleDirectory = new File("Z:/n2c2/data/samplesTraining");
-		List<File> sampleFiles = (List<File>) FileUtils.listFiles(sampleDirectory, TrueFileFilter.INSTANCE,
-				TrueFileFilter.INSTANCE);
-
-		DataUtilities utilities = new DataUtilities();
-
-		List<Patient> patients;
-		List<String> sentences;
-		try {
-			patients = new ArrayList<Patient>();
-			for (File patientSample : sampleFiles) {
-				patients.add(new PatientDAO().fromXML(patientSample));
-			}
-
-			System.out.println(patients.get(200).getID());
-			sentences = DataUtilities.getSentences(patients.get(200).getText());
-
-			for (String sentence : sentences) {
-				String normalized = utilities.processTextReduced(sentence);
-				String char3Grams001 = utilities.getChar3GramRepresentation(normalized);
-				System.out.println(sentence + "\t" + normalized + "\t" + char3Grams001);
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		}
-		assertEquals(true, true);
-	}
 }
