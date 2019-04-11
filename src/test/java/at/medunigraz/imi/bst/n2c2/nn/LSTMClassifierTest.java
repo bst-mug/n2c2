a781c35044f655c0842b72fe44a34a37a04883e0
Michel Oleynik <michelole@gmail.com>
2019-04-11 10:22:51 +0200

    Partial revert of f99b831cba360ddd3549bb15e292d8da92b94c20 (refs #82)

---
+++ src/test/java/at/medunigraz/imi/bst/n2c2/nn/LSTMClassifierTest.java
@@ -0,0 +1,111 @@
+package at.medunigraz.imi.bst.n2c2.nn;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.junit.Ignore;
+import org.xml.sax.SAXException;
+
+import at.medunigraz.imi.bst.n2c2.dao.PatientDAO;
+import at.medunigraz.imi.bst.n2c2.model.Criterion;
+import at.medunigraz.imi.bst.n2c2.model.Patient;
+
+public class LSTMClassifierTest {
+
+	@Ignore
+	public void train() {
+
+		// read in patients
+		File sampleDirectory = new File("Z:/n2c2/data/samplesTraining");
+		List<File> sampleFiles = (List<File>) FileUtils.listFiles(sampleDirectory, TrueFileFilter.INSTANCE,
+				TrueFileFilter.INSTANCE);
+
+		List<Patient> patients;
+		try {
+			patients = new ArrayList<Patient>();
+			for (File patientSample : sampleFiles) {
+				patients.add(new PatientDAO().fromXML(patientSample));
+			}
+
+			LSTMClassifier classifier = new LSTMClassifier(patients);
+			classifier.train(patients);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+
+		assertEquals(true, true);
+	}
+
+	@Ignore
+	public void initializeNetworkFromFile() {
+
+		String modelName = "N2c2BILSTM_MBL_Full_50.zip";
+		LSTMClassifier classifier = new LSTMClassifier();
+		classifier.initializeNetworkFromFile(modelName);
+
+		assertEquals(true, true);
+	}
+
+	@Ignore
+	public void predictCriterion() {
+		// read in patients
+		File sampleDirectory = new File("Z:/n2c2/data/samplesTraining");
+		List<File> sampleFiles = (List<File>) FileUtils.listFiles(sampleDirectory, TrueFileFilter.INSTANCE,
+				TrueFileFilter.INSTANCE);
+
+		String modelName = "N2c2BILSTM_MBL_Full_50.zip";
+		String pathToWordVectors = "C:\\Users\\Markus\\Downloads\\GoogleNews-vectors-negative300.bin.gz";
+
+		List<Patient> patients;
+		try {
+			patients = new ArrayList<Patient>();
+			for (File patientSample : sampleFiles) {
+				patients.add(new PatientDAO().fromXML(patientSample));
+
+			}
+			LSTMClassifier classifier = new LSTMClassifier(pathToWordVectors, modelName);
+			classifier.predict(patients.get(0), Criterion.ABDOMINAL);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true, true);
+	}
+
+	@Ignore
+	public void predictPatientList() {
+		// read in patients
+		File sampleDirectory = new File("Z:/n2c2/data/samplesTraining");
+		List<File> sampleFiles = (List<File>) FileUtils.listFiles(sampleDirectory, TrueFileFilter.INSTANCE,
+				TrueFileFilter.INSTANCE);
+
+		String modelName = "N2c2BILSTM_MBL_Full_50.zip";
+		String pathToWordVectors = "C:\\Users\\Markus\\Downloads\\GoogleNews-vectors-negative300.bin.gz";
+
+		List<Patient> patients;
+		try {
+			patients = new ArrayList<Patient>();
+			for (File patientSample : sampleFiles) {
+				patients.add(new PatientDAO().fromXML(patientSample));
+
+			}
+			LSTMClassifier classifier = new LSTMClassifier(pathToWordVectors, modelName);
+			classifier.predict(patients);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+		assertEquals(true, true);
+	}
+}
