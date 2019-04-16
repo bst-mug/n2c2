c91188a8632302a2ed39be1de37398ce7ed685d6
Michel Oleynik <michelole@gmail.com>
2019-04-16 12:25:09 +0200

    Add test to `LSTMClassifier` (refs #82)

--- src/test/java/at/medunigraz/imi/bst/n2c2/nn/LSTMClassifierTest.java
+++ src/test/java/at/medunigraz/imi/bst/n2c2/nn/LSTMClassifierTest.java
@@ -1,6 +1,8 @@
 package at.medunigraz.imi.bst.n2c2.nn;
 
-public class LSTMClassifierTest {
+public class LSTMClassifierTest extends BaseNNClassifierTest {
 
-
+    public LSTMClassifierTest() {
+        this.classifier = new LSTMClassifier();
+    }
 }
