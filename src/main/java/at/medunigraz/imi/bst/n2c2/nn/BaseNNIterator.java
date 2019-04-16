567fa01868353a85eed89db05f963af42c536901
Michel Oleynik <michelole@gmail.com>
2019-04-16 15:00:27 +0200

    DRY Inititalize `binaryMultiHotVector` in a for loop

--- src/main/java/at/medunigraz/imi/bst/n2c2/nn/BaseNNIterator.java
+++ src/main/java/at/medunigraz/imi/bst/n2c2/nn/BaseNNIterator.java
@@ -26,46 +26,9 @@ public abstract class BaseNNIterator implements DataSetIterator {
      * @param binaryMultiHotVector
      */
     protected void fillBinaryMultiHotVector(List<Boolean> binaryMultiHotVector) {
-
-        // <ABDOMINAL met="not met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.ABDOMINAL).equals(Eligibility.MET));
-
-        // <ADVANCED-CAD met="met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.ADVANCED_CAD).equals(Eligibility.MET));
-
-        // <ALCOHOL-ABUSE met="not met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.ALCOHOL_ABUSE).equals(Eligibility.MET));
-
-        // <ASP-FOR-MI met="met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.ASP_FOR_MI).equals(Eligibility.MET));
-
-        // <CREATININE met="not met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.CREATININE).equals(Eligibility.MET));
-
-        // <DIETSUPP-2MOS met="met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.DIETSUPP_2MOS).equals(Eligibility.MET));
-
-        // <DRUG-ABUSE met="not met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.DRUG_ABUSE).equals(Eligibility.MET));
-
-        // <ENGLISH met="met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.ENGLISH).equals(Eligibility.MET));
-
-        // <HBA1C met="met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.HBA1C).equals(Eligibility.MET));
-
-        // <KETO-1YR met="not met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.KETO_1YR).equals(Eligibility.MET));
-
-        // <MAJOR-DIABETES met="met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.MAJOR_DIABETES).equals(Eligibility.MET));
-
-        // <MAKES-DECISIONS met="met" />
-        binaryMultiHotVector
-                .add(patients.get(cursor).getEligibility(Criterion.MAKES_DECISIONS).equals(Eligibility.MET));
-
-        // <MI-6MOS met="met" />
-        binaryMultiHotVector.add(patients.get(cursor).getEligibility(Criterion.MI_6MOS).equals(Eligibility.MET));
+        for (Criterion criterion : Criterion.classifiableValues()) {
+            binaryMultiHotVector.add(patients.get(cursor).getEligibility(criterion).equals(Eligibility.MET));
+        }
     }
 
     /*
